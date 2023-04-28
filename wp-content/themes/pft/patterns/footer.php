a6b4f47f23973edd32c39c00bc02dbce517fbdf6
victorkane <victorkane@gmail.com>
2023-04-28 05:23:30 -0300

    style(template parts): Add logo and some initial padding to header and footer

---
+++ wp-content/themes/pft/patterns/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: footer
+ * Slug: pft/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="padding-top:80px;padding-bottom:30px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|50","bottom":"0","left":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"padding":{"left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal"}} -->
+<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:image {"id":66,"width":85,"height":85,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/icon170.png" alt="" class="wp-image-66" width="85" height="85"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fixed","flexSize":"1rem"}}} -->
+<p class="has-text-align-left">©</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:epico/dynamic-year-block /-->
+
+<!-- wp:paragraph -->
+<p>authored by Victor Kane CC-BY-SA (unless otherwise stated) Creative Commons <a href="https://creativecommons.org/licenses/by-sa/4.0/">Attribution-ShareAlike 4.0 International License</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
