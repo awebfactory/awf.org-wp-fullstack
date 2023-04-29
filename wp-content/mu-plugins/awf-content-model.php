a2b2d2357791c9ee3cbcc4c1e69e39efa90d27f2
victorkane <victorkane@gmail.com>
2023-04-29 16:17:58 -0300

    feat(cpt): enable non-hierarchical taxonomy Tag for custom post type Conversation

--- wp-content/mu-plugins/awf-content-model.php
+++ wp-content/mu-plugins/awf-content-model.php
@@ -50,7 +50,7 @@ function awebfactory_content_model() {
       }
     ));
   }
-    register_post_type(
+  register_post_type(
     'conversation',
     array(
       'labels'       => array(
@@ -71,6 +71,32 @@ function awebfactory_content_model() {
       ),
     )
   );
+  /* See [How to Add Tags to Custom Post Type?](https://wordpress.stackexchange.com/questions/62260/how-to-add-tags-to-custom-post-type) */
+  register_taxonomy('tag','conversation',array(
+    'hierarchical' => false,
+    'labels' => array(
+      'name' => _x( 'Tags', 'taxonomy general name' ),
+      'singular_name' => _x( 'Tag', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Tags' ),
+      'popular_items' => __( 'Popular Tags' ),
+      'all_items' => __( 'All Tags' ),
+      'parent_item' => null,
+      'parent_item_colon' => null,
+      'edit_item' => __( 'Edit Tag' ), 
+      'update_item' => __( 'Update Tag' ),
+      'add_new_item' => __( 'Add New Tag' ),
+      'new_item_name' => __( 'New Tag Name' ),
+      'separate_items_with_commas' => __( 'Separate tags with commas' ),
+      'add_or_remove_items' => __( 'Add or remove tags' ),
+      'choose_from_most_used' => __( 'Choose from the most used tags' ),
+      'menu_name' => __( 'Tags' )
+    ),
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'tag' ),
+  ));
 }
 
 add_action('init', 'awebfactory_content_model');
