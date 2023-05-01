7268c06aec4a50faff4585e0389cbd8a56de5bfe
victorkane <victorkane@gmail.com>
2023-05-01 07:54:53 -0300

    feat(theme patterns): Use WP Engine Pattern Manager to create a third Team category pattern specifically for project cpt, Project Conversations, with new project pattern suggestion, to list conversations if you enter specific cpt tag, best use is project short name

---
+++ wp-content/themes/pft/patterns/project-conversations.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Project Conversations
+ * Slug: project-conversations
+ * Description: 
+ * Categories: team
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: project
+ * Inserter: true
+ */
+
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">Project Conversations</h2>
+<!-- /wp:heading -->
+
+<!-- wp:query {"queryId":11,"query":{"perPage":6,"pages":0,"offset":0,"postType":"conversation","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"tag":[8]},"parents":[]},"displayLayout":{"type":"flex","columns":3},"align":"full","layout":{"type":"constrained","contentSize":"","wideSize":"1920px","justifyContent":"center"}} -->
+<div class="wp-block-query alignfull"><!-- wp:post-template -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"layout":{"inherit":false}} -->
+<div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:post-title {"isLink":true} /-->
+
+<!-- wp:post-excerpt /-->
+
+<!-- wp:post-date /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group -->
