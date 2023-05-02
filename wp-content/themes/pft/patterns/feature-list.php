999dd03b4386dbe534f5bfd5a6fa5bea4c8b84f3
victorkane <victorkane@gmail.com>
2023-05-02 10:26:18 -0300

    feat(theme): added two more project patterns (feature list and kanban board) for project and conversations use thanks to wp engine pattern manager

---
+++ wp-content/themes/pft/patterns/feature-list.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Feature List
+ * Slug: feature-list
+ * Description: 
+ * Categories: team
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: project
+ * Inserter: true
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"color":{"background":"#e2e8ed"}}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#e2e8ed"><!-- wp:spacer {"height":"4rem"} -->
+<div style="height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Feature List</h2>
+<!-- /wp:heading -->
+
+<!-- wp:table {"hasFixedLayout":true,"align":"wide","style":{"color":{"background":"#f2f5f8"}},"borderColor":"black","className":"is-style-stripes"} -->
+<figure class="wp-block-table alignwide is-style-stripes"><table class="has-background has-border-color has-black-border-color has-fixed-layout" style="background-color:#f2f5f8"><thead><tr><th>We will create this <em>feature</em></th><th>for this <em>persona</em></th><th>in order to achieve this <em>outcome</em></th></tr></thead><tbody><tr><td><strong>User registration</strong></td><td><strong>All users</strong></td><td>Allow users to create accounts and access courses</td></tr><tr><td><strong>Course catalog</strong></td><td><strong>All users</strong></td><td>Display a list of available courses</td></tr><tr><td><strong>Course search</strong></td><td><strong>All users</strong></td><td>Enable users to search for specific courses</td></tr><tr><td><strong>Course details</strong></td><td><strong>All users</strong></td><td>Provide detailed information about each course</td></tr><tr><td><strong>Enroll in a course</strong></td><td><strong>All users</strong></td><td>Allow users to enroll in their chosen courses</td></tr><tr><td><strong>Progress tracking</strong></td><td><strong>Enrolled users</strong></td><td>Track and display user progress within a course</td></tr><tr><td><strong>Discussion forums</strong></td><td><strong>Enrolled users</strong></td><td>Facilitate communication and collaboration among users</td></tr><tr><td><strong>Instructor interaction</strong></td><td><strong>Enrolled users</strong></td><td>Provide a means for students to communicate with instructors</td></tr><tr><td><strong>Payment gateway</strong></td><td><strong>All users</strong></td><td>Enable secure online payment for course enrollment</td></tr><tr><td><strong>Multilingual support</strong></td><td><strong>International users</strong></td><td>Provide language options for non-English speakers</td></tr></tbody><tfoot><tr><td>We will create this <em>feature</em></td><td>for this <em>persona</em></td><td>in order to achieve this <em>outcome</em></td></tr></tfoot></table></figure>
+<!-- /wp:table -->
+
+<!-- wp:spacer {"height":"4rem"} -->
+<div style="height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>Based on <a href="https://wordpress.org/patterns/pattern/schedule-table/">Schedule Table</a> pattern by <a href="https://wordpress.org/patterns/author/vheinrich/">vheinrich</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ wp-content/themes/pft/patterns/kanban-board.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Title: Kanban Board
+ * Slug: kanban-board
+ * Description: 
+ * Categories: team
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: conversation, project
+ * Inserter: true
+ */
+
+?>
+<!-- wp:heading {"textAlign":"left","style":{"spacing":{"margin":{"top":"30px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<h2 class="wp-block-heading has-text-align-left" style="margin-top:30px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>Kanboard Board</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">May be used as a single WIP board, or else have multiple boards (change this paragraph into a date for board snapshot)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"align":"wide","className":"is-style-wide"} -->
+<hr class="wp-block-separator alignwide has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"}},"borderColor":"cyan-bluish-gray"} -->
+<div class="wp-block-group has-border-color has-cyan-bluish-gray-border-color" style="border-width:2px"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"luminous-vivid-amber","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-luminous-vivid-amber-background-color has-background has-medium-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>Up Next</strong> (4)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"10px","bottom":"10px","left":"10px"}}}} -->
+<div class="wp-block-group" style="padding-top:0px;padding-right:10px;padding-bottom:10px;padding-left:10px"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>First Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Second Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Third Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Fourth Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"}},"borderColor":"luminous-vivid-amber"} -->
+<div class="wp-block-group has-border-color has-luminous-vivid-amber-border-color" style="border-width:2px"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"luminous-vivid-amber","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-luminous-vivid-amber-background-color has-background has-medium-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>Working</strong> (3)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"10px","bottom":"10px","left":"10px"}}}} -->
+<div class="wp-block-group" style="padding-top:0px;padding-right:10px;padding-bottom:10px;padding-left:10px"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>First Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Second Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Third Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"}},"borderColor":"vivid-green-cyan"} -->
+<div class="wp-block-group has-border-color has-vivid-green-cyan-border-color" style="border-width:2px"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"luminous-vivid-amber","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-luminous-vivid-amber-background-color has-background has-medium-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>Done</strong> (2)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"10px","bottom":"10px","left":"10px"}}}} -->
+<div class="wp-block-group" style="padding-top:0px;padding-right:10px;padding-bottom:10px;padding-left:10px"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>First Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"color":{"background":"#f9f9f9"}},"fontSize":"small"} -->
+<p class="has-background has-small-font-size" style="background-color:#f9f9f9"><strong>Second Item<br></strong>Description of the item that will give more context.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><em>Pattern based on <a href="https://wordpress.org/patterns/pattern/kanban-board/">Kanban Board</a> by <a href="https://wordpress.org/patterns/author/dustinwstout">Dustin W. Stout</a></em></p>
+<!-- /wp:paragraph -->
